c5de930babe3aa820a996ffd8982610a098af1a8
Levent KÖYBAŞI <leventkoybasi@hotmail.com>
2025-04-27 16:18:21 -0700

    readme updated

--- src/constant/constantContact.js
+++ src/constant/constantContact.js
@@ -15,8 +15,3 @@ export const CONTACT_SORTABLE_FIELDS = [
 ];
 export const FIFTEEN_MINUITES = 15 * 60 * 1000; // for access token
 export const ONE_DAY = 24 * 60 * 60 * 1000; // for refresh token
-
-export const USER_ROLES = {
-  TEACHER: 'teacher',
-  PARENT: 'parent',
-};
